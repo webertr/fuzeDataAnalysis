4bf439b93091fc3dce2e30997cd085aaf2db0d7c
Tobin Weber <webertr@uw.edu>
2018-02-16 07:59:12 -0800

    setting up rename script

--- script/renameShot.sh
+++ script/renameShot.sh
@@ -1,8 +1,15 @@
-a=11
+startPulse=1
+startFile=1
+numImages=10
 
-for i in *.JPG; do
-    new=$(printf "1802150%2d.JPG" "$a") #04 pad to length of 4
-    echo $new
-    mv -i -- "$i" "$new"
-    let a=a+1
+for i in `seq 1 $numImages`; 
+do
+
+    newFile=$(printf "1802150%02d.JPG" "$startPulse")
+    oldFile=$(printf "DSC_00%02d.JPG" "$startFile")
+    mv -i -- "$oldFile" "$newFile"
+    echo "Moving $oldFile to $newFile"
+    let startPulse=startPulse+1
+    let startFile=startFile+1
+
 done
