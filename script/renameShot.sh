6cd29872b1ef5dfbd1605c18098493eba1bf45ef
Tobin Weber <webertr@uw.edu>
2018-03-07 15:17:53 -0800

    change to handle baseline shots

--- script/renameShot.sh
+++ script/renameShot.sh
@@ -1,7 +1,7 @@
-fileBase=180222
-startPulse=180222023
-startFile=23
-numImages=27
+fileBase=180307
+startPulse=180307024
+startFile=1
+numImages=6
 
 for i in `seq 1 $numImages`; 
 do
@@ -10,7 +10,12 @@ do
     oldFile=$(printf "/home/fuze/DHI_Images/%s/DSC_00%02d.JPG" "$fileBase" "$startFile")
     mv -i -- "$oldFile" "$newFile"
     echo "Moving $oldFile to $newFile"
-    let startPulse=startPulse+1
     let startFile=startFile+1
+    newFile=$(printf "/home/fuze/DHI_Images/%s/%d_Baseline.JPG" "$fileBase" "$startPulse")
+    oldFile=$(printf "/home/fuze/DHI_Images/%s/DSC_00%02d.JPG" "$fileBase" "$startFile")
+    mv -i -- "$oldFile" "$newFile"
+    echo "Moving $oldFile to $newFile"
+    let startFile=startFile+1 
+    let startPulse=startPulse+1
 
 done
