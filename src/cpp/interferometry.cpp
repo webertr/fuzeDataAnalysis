42ae13821174477e83b19b8ac2108fe8313ef373
Tobin Weber <webertr@uw.edu>
2019-09-06 09:54:34 -0700

    This is currently working. Still adding in more analysis of IF data

--- src/cpp/interferometry.cpp
+++ src/cpp/interferometry.cpp
@@ -6,54 +6,168 @@
  *
  ******************************************************************************/
 
-static gsl_vector *getIFData(long shotNumber, long baseLineShotNumber, int chordNum);
+static double getCosOffset(long baseLineShotNumber, int chordNum);
+static double getSinOffset(long baseLineShotNumber, int chordNum);
+static double getInitialPhase(gsl_vector *cosVec, gsl_vector *sinVec);
+static void rotate2DSignal(gsl_vector **xVec, gsl_vector **yVec, double phase);
+static gsl_vector *getCosData(long shotNumber, int chordNum);
+static gsl_vector *getSinData(long shotNumber, int chordNum);
 
 
 /******************************************************************************
- * Function: getIFData
+ * Function: getCosData
  * Inputs: 
  * Returns: boolean
- * Description: Gets inteferometry data for passed shot number
+ * Description: Gets the cos(phi(t)) data.
+ ******************************************************************************/
+
+static gsl_vector *getCosData(long shotNumber, int chordNum) {
+  
+  std::string cNode;
+  std::ostringstream oss;
+
+  oss << "\\TOP.SIGNALS.DENSITY.HENE_IF:NE_" << chordNum << ":COS" << chordNum;
+  cNode = oss.str();
+  oss.str("");
+
+  gsl_vector *cosData = readMDSplusVector(shotNumber, cNode, "fuze");
+
+  return cosData;
+  
+}
+
+
+/******************************************************************************
+ * Function: getSinData
+ * Inputs: 
+ * Returns: boolean
+ * Description: Gets the sin(phi(t)) data.
+ ******************************************************************************/
+
+static gsl_vector *getSinData(long shotNumber, int chordNum) {
+  
+  std::string sNode;
+  std::ostringstream oss;
+
+  oss << "\\TOP.SIGNALS.DENSITY.HENE_IF:NE_" << chordNum << ":SIN" << chordNum;
+  sNode = oss.str();
+  oss.str("");
+
+  gsl_vector *sinData = readMDSplusVector(shotNumber, sNode, "fuze");
+
+  return sinData;
+  
+}
+
+/******************************************************************************
+ * Function: getInitialPhase
+ * Inputs: 
+ * Returns: boolean
+ * Description: Calculates the initial phase of the interference data
+ ******************************************************************************/
+
+static double getInitialPhase(gsl_vector *cosVec, gsl_vector *sinVec) {
+  
+  double cos1, sin1;
+
+  cos1 = gsl_vector_get(cosVec, 0);
+  sin1 = gsl_vector_get(sinVec, 0);
+
+  double initPhase = atan2(sin1, cos1);
+
+  return initPhase;
+  
+}
+
+
+/******************************************************************************
+ * Function: getCosOffset
+ * Inputs: 
+ * Returns: boolean
+ * Description: Calculates inteferometry data baseline offset for passed 
+ * baseline shot number
  * TOP.SIGNALS.DENSITY.HENE_IF:NE_#:COS# = The cos of the phase shift of the scene beam
  * TOP.SIGNALS.DENSITY.HENE_IF:NE_#:SIN# = The sin of the phase shift of the scene beam
  ******************************************************************************/
 
-static gsl_vector *getIFData(long shotNumber, long baseLineShotNumber, int chordNum) {
-
+static double getCosOffset(long baseLineShotNumber, int chordNum) {
   
   std::string baseLineCNode;
-  std::string baseLineSNode;
   std::ostringstream oss;
 
   oss << "\\TOP.SIGNALS.DENSITY.HENE_IF:NE_" << chordNum << ":COS" << chordNum;
   baseLineCNode = oss.str();
   oss.str("");
 
-  oss << "\\TOP.SIGNALS.DENSITY.HENE_IF:NE_" << chordNum << ":SIN" << chordNum;
-  baseLineSNode = oss.str();
-  oss.str("");
-
   gsl_vector *baseLineCos = readMDSplusVector(baseLineShotNumber, baseLineCNode, "fuze");
-  gsl_vector *baseLineSin = readMDSplusVector(baseLineShotNumber, baseLineSNode, "fuze");
 
   double cosOffset = gsl_stats_mean(baseLineCos->data, baseLineCos->size, baseLineCos->stride);
-  double sinOffset = gsl_stats_mean(baseLineSin->data, baseLineSin->size, baseLineSin->stride);
-
-  std::cout << "X offset: " << cosOffset << "\n";
-  std::cout << "Y offset: " << sinOffset << "\n";
-
-  plotVectorData(baseLineCos, baseLineSin, "", "", "data/inter.txt", "data/inter.sh");
-
-  //gsl_vector_add_constant(cos1, -cos1Offset);
-  //gsl_vector_add_constant(sin1, -sin1Offset);
-
-  //plotVectorData(cos1, sin1, "", "", "data/inter.txt", "data/inter.sh");
 
   gsl_vector_free(baseLineCos);
+
+  return cosOffset;
+  
+}
+
+/******************************************************************************
+ * Function: getSinOffset
+ * Inputs: 
+ * Returns: boolean
+ * Description: Calculates inteferometry data baseline offset for passed 
+ * baseline shot number
+ * TOP.SIGNALS.DENSITY.HENE_IF:NE_#:SIN# = The sin of the phase shift of the scene beam
+ * TOP.SIGNALS.DENSITY.HENE_IF:NE_#:SIN# = The sin of the phase shift of the scene beam
+ ******************************************************************************/
+
+static double getSinOffset(long baseLineShotNumber, int chordNum) {
+  
+  std::string baseLineCNode;
+  std::ostringstream oss;
+
+  oss << "\\TOP.SIGNALS.DENSITY.HENE_IF:NE_" << chordNum << ":SIN" << chordNum;
+  baseLineCNode = oss.str();
+  oss.str("");
+
+  gsl_vector *baseLineSin = readMDSplusVector(baseLineShotNumber, baseLineCNode, "fuze");
+
+  double sinOffset = gsl_stats_mean(baseLineSin->data, baseLineSin->size, baseLineSin->stride);
+
   gsl_vector_free(baseLineSin);
 
-  return (gsl_vector *) NULL;
+  return sinOffset;
   
+}
+
+
+/******************************************************************************
+ * Function: rotate2DSignal
+ * Inputs: 
+ * Returns: void
+ * Description: Rotates the 2D x, y data. Modifies the passed vectors.
+ ******************************************************************************/
+
+static void rotate2DSignal(gsl_vector **xVec, gsl_vector **yVec, double phase) {
+  
+  double tempX, tempY, tempXp, tempYp;
+  int ii;
+
+
+  for (ii = 0; ii < (int) (*xVec)->size; ii++) {
+
+    tempX = gsl_vector_get(*xVec, ii);
+    tempY = gsl_vector_get(*yVec, ii);
+    tempXp = tempX * cos(phase) + tempY * sin(phase);
+    tempYp = -tempX * sin(phase) + tempY * cos(phase);
+
+    gsl_vector_set(*xVec, ii, tempXp);
+    gsl_vector_set(*yVec, ii, tempYp);
+
+  }
+
+
+
+  return;
+
 }
 
 
@@ -63,12 +177,24 @@ static gsl_vector *getIFData(long shotNumber, long baseLineShotNumber, int chord
  *
  ******************************************************************************/
 
-static bool testGetIFData();
+static bool testGetOffset();
+static bool testGetInitialPhase();
+static bool testRotate2DSignal();
 
 bool testInterferometry() {
 
-  if (!testGetIFData()) {
-    std::cout << "Test interferometry.cpp get data FAILED\n";
+  if (!testGetOffset()) {
+    std::cout << "Test interferometry.cpp get offset FAILED\n";
+    return false;
+  }
+
+  if (!testGetInitialPhase()) {
+    std::cout << "Test interferometry.cpp get initial phase FAILED\n";
+    return false;
+  }
+
+  if (!testRotate2DSignal()) {
+    std::cout << "Test interferometry.cpp rotate 2D signal FAILED\n";
     return false;
   }
 
@@ -79,12 +205,71 @@ bool testInterferometry() {
 }
 
 
-bool testGetIFData() { 
+bool testGetOffset() { 
 
-  getIFData(190903008, 190903004, 1);
-  //getIFData(190903005);
-  //getIFData(190416004);
+  double cosOffset, sinOffset;
+
+  cosOffset = getCosOffset(190903004, 1);
+
+  sinOffset = getSinOffset(190903004, 1);
+
+  if ( (cosOffset > .014) && (cosOffset < .015) && (sinOffset > .02) && (sinOffset < .03)) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+bool testGetInitialPhase() { 
+
+  double initPhase;
+  double cosOffset, sinOffset;
+
+  cosOffset = getCosOffset(190903004, 1);
+  sinOffset = getSinOffset(190903004, 1);
+
+  gsl_vector *cosVec = getCosData(190903007, 1);
+  gsl_vector *sinVec = getSinData(190903007, 1);
+
+  gsl_vector_add_constant(cosVec, -cosOffset);
+  gsl_vector_add_constant(sinVec, -sinOffset);
+
+  initPhase = getInitialPhase(cosVec, sinVec);
+
+  if ( (initPhase > 1.4) && (initPhase < 1.5) ) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+
+bool testRotate2DSignal() { 
+
+  double initPhase;
+  double cosOffset, sinOffset;
+
+  cosOffset = getCosOffset(190903004, 1);
+  sinOffset = getSinOffset(190903004, 1);
+
+  gsl_vector *cosVec = getCosData(190903007, 1);
+  gsl_vector *sinVec = getSinData(190903007, 1);
+
+  gsl_vector_add_constant(cosVec, -cosOffset);
+  gsl_vector_add_constant(sinVec, -sinOffset);
+
+  initPhase = getInitialPhase(cosVec, sinVec);
+
+  rotate2DSignal(&cosVec, &sinVec, initPhase);
+
+  initPhase = getInitialPhase(cosVec, sinVec);
   
-  return true;
+  if ( (initPhase > -0.01) && (initPhase < 0.01) ) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
