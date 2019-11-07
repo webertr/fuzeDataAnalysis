ba84737cfb651f149fbd0153c012b647f7ef9448
Tobin Weber <webertr@uw.edu>
2019-11-07 11:52:38 -0800

    Added in all 6 ignitron modules

--- ngspice/ignitronBank/gnuplot_script.sh
+++ ngspice/ignitronBank/gnuplot_script.sh
@@ -8,8 +8,8 @@ set xrange [0:200]
 
 plot 'ip_convert.txt' using ($1*1E6):2 title "I_{P}",\
      'ic1_convert.txt' using ($1*1E6):2 title "I_{C1}",\
-     'iout1_convert.txt' using ($1*1E6):2 title "I_{OUT}",\
-     'icb1_convert.txt' using ($1*1E6):2 title "I_{CB}"
+     'iout1_convert.txt' using ($1*1E6):2 title "I_{OUT1}",\
+     'icb1_convert.txt' using ($1*1E6):2 title "I_{CB1}"
 
 
 #set term x11 1
