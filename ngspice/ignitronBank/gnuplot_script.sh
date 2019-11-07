f802d146ef980a4125c829c9160b1bea2de1420a
Tobin Weber <webertr@uw.edu>
2019-11-07 11:31:49 -0800

    this is kind of working

--- ngspice/ignitronBank/gnuplot_script.sh
+++ ngspice/ignitronBank/gnuplot_script.sh
@@ -4,18 +4,19 @@ set term x11 0
 
 set ylabel "I (kA)" font ",16" offset -1,0
 set xlabel "time (usec)" font ",16" offset 0,0
+set xrange [0:200]
 
-plot 'ic1_convert.txt' using ($1*1E6):2 title "I 1 Total",\
-     'ic1_convert.txt' using ($1*1E6):2 title "I C1",\
-     'ic1_convert.txt' using ($1*1E6):2 title "I D1",\
-     'ic1_convert.txt' using ($1*1E6):2 title "I P"
+plot 'ip_convert.txt' using ($1*1E6):2 title "I_{P}",\
+     'ic1_convert.txt' using ($1*1E6):2 title "I_{C1}",\
+     'iout1_convert.txt' using ($1*1E6):2 title "I_{OUT}",\
+     'icb1_convert.txt' using ($1*1E6):2 title "I_{CB}"
 
-set term x11 1
 
-set ylabel "V_{Gap} (kV)" font ",16" offset -1,0
-set xlabel "time (usec)" font ",16" offset 0,0
+#set term x11 1
 
-plot 'ic1_convert.txt' using ($1*1E6):2 title "V_{GapR}",\
-     'ic1_convert.txt' using ($1*1E6):2 title "V_{GapL}"
+#set ylabel "V_{Gap} (kV)" font ",16" offset -1,0
+#set xlabel "time (usec)" font ",16" offset 0,0
+
+#plot 'vgap_convert.txt' using ($1*1E6):2 title "V_{Gap}"
 
 pause -1
