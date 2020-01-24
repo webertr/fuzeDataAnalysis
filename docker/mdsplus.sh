9289d958b20daa7d6041d119990f0025de6b301b
Tobin R. Weber <webertr@uw.edu>
2020-01-23 16:57:29 -0800

    Finally got the docker thing working.

---
+++ docker/mdsplus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# MDSPLUS
+export fuze_path=fuze.fuze::/usr/local/trees/fuze/newdata/~t
+export fdigitizers_path=fuze.fuze::/usr/local/trees/fuze/newdata/~t
+export fanalysis_path=fuze.fuze::/usr/local/trees/fuze/newdata/~t
+
+export MDS_PATH="/usr/local/UWmdsplus/tdi;/usr/local/mdsplus/tdi"
+
+export zaphd_path=zappa.zap::/usr/local/trees/zaphd/new/~t
+#export zaphd_path=zappa.zap::/usr/local/trees/zaphd/models/~t >;
+
+export digitizershd_path=zappa.zap::/usr/local/trees/zaphd/new/~t
+#export digitizershd_path=zappa.zap::/usr/local/trees/zaphd/models/~t >;
+
+export analysishd_path=zappa.zap::/usr/local/trees/zaphd/new/~t
+#export analysishd_path=zappa.zap::/usr/local/trees/zaphd/models/~t >;
